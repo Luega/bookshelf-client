85c3a51502f4a6d9a9624f49a800dd058570f60f
luega <lucamartinelli.developer@gmail.com>
2024-01-18 14:57:16 +0100

    Create components structure. Add required imports.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,43 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookshelfComponent } from './pages/bookshelf/bookshelf.component';
+import { BookDetailsComponent } from './pages/book-details/book-details.component';
+import { BookAddComponent } from './pages/book-add/book-add.component';
+import { BookEditComponent } from './pages/book-edit/book-edit.component';
+import { QuotesComponent } from './pages/quotes/quotes.component';
+import { Page404Component } from './pages/page404/page404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: "",
+    redirectTo: "/books",
+    pathMatch: 'full'
+  },
+  { 
+    path: "books",
+    component: BookshelfComponent,
+  },
+  { 
+    path: "books/:id",
+    component: BookDetailsComponent
+  },
+  { 
+    path: "books/add-book",
+    component: BookAddComponent
+  },
+  { 
+    path: "books/edit-book",
+    component: BookEditComponent
+  },
+  { 
+    path: "quotes",
+    component: QuotesComponent
+  },
+  { 
+    path: "**",
+    component: Page404Component
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
+import { BookshelfComponent } from './pages/bookshelf/bookshelf.component';
+import { QuotesComponent } from './pages/quotes/quotes.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BookDetailsComponent } from './pages/book-details/book-details.component';
+import { Page404Component } from './pages/page404/page404.component';
+import { CardComponent } from './components/card/card.component';
+import { ButtonComponent } from './components/button/button.component';
+import { BookAddComponent } from './pages/book-add/book-add.component';
+import { BookEditComponent } from './pages/book-edit/book-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookshelfComponent,
+    BookDetailsComponent,
+    BookAddComponent,
+    BookEditComponent,
+    QuotesComponent,
+    Page404Component,
+    HeaderComponent,
+    FooterComponent,
+    CardComponent,
+    ButtonComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

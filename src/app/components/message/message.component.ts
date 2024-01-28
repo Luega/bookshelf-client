7daa172daa270e8b67fac8d74997b644eb7ca14d
luega <lucamartinelli.developer@gmail.com>
2024-01-28 19:04:50 +0100

    Style message component.

--- src/app/components/message/message.component.ts
+++ src/app/components/message/message.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { MessageType } from 'src/app/models/message/message-type.enum';
 import { IMessage } from 'src/app/models/message/message.interface';
 
 @Component({
@@ -6,6 +7,30 @@ import { IMessage } from 'src/app/models/message/message.interface';
   templateUrl: './message.component.html',
   styleUrls: ['./message.component.css']
 })
-export class MessageComponent {
+export class MessageComponent implements OnInit {
   @Input() message?: IMessage;
+  isErrorType: boolean = false;
+  isOptionType: boolean = false;
+  isSuccessType: boolean = false;
+
+  ngOnInit(): void {
+    console.log(this.message?.type);
+    if (this.message) {
+      switch (this.message.type) {
+        case MessageType.error:
+          this.isErrorType = true;
+          break;
+  
+        case MessageType.option:
+          this.isOptionType = true;
+          break;
+  
+        case MessageType.success:
+          this.isSuccessType = true;
+          break;
+      }
+    }
+    console.log(this.isOptionType);
+    
+  }
 }
